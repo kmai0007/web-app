04ed09d98ceb14843fd9d3835d5688b66317ee74
khoi mai <87831919+kmai0007@users.noreply.github.com>
2021-08-06 15:07:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ app.use(
     authRequired: false,
     auth0Logout: true,
     baseURL: APP_URL,
+    authorizationParams: {
+      response_type: "code id_token",
+      audience: "https://expenses-api",
+      scope: "openid profile email read:reports",
+    },
   })
 );
 
